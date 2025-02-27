774fc5583b9f601a65d3994be82883f14b766726
Jeremy Nicholl <nicholljeremy@gmail.com>
2025-02-27 14:52:48 -0500

    Partial updates for grabber to add encoder and config for wrist motor

--- src/main/java/frc/robot/subsystems/grabber/GrabberIOReal.java
+++ src/main/java/frc/robot/subsystems/grabber/GrabberIOReal.java
@@ -5,6 +5,7 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj.AnalogInput;
@@ -18,19 +19,31 @@ public class GrabberIOReal implements GrabberIO {
   protected final SparkMax rightGrabberMotor = new SparkMax(Constants.CAN.GRABBER_RIGHT_MOTOR, MotorType.kBrushless);
   protected final SparkMax wristMotor = new SparkMax(Constants.CAN.GRABBER_WRIST_MOTOR, MotorType.kBrushless);
 
+  protected final RelativeEncoder wristEncoder = wristMotor.getEncoder();
+
   protected final DigitalInput homeSwitch = new DigitalInput(Constants.Digital.ALGAE_HOME_SWITCH);
   protected final AnalogInput distanceSensor = new AnalogInput(Constants.Analog.ALGAE_DISTANCE_SENSOR);
 
   public GrabberIOReal() {
-    SparkMaxConfig config = new SparkMaxConfig();
-    config.idleMode(IdleMode.kBrake).smartCurrentLimit(38).voltageCompensation(12.0);
+    SparkMaxConfig grabberConfig = new SparkMaxConfig();
 
-    config.inverted(Constants.Grabber.LEFT_GRABBER_INVERTED);
-    REVUtils.tryUntilOk(() -> leftGrabberMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+    // NEO 550's have a recommended current limit range of 20-40A.
+    grabberConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(30).voltageCompensation(12.0);
+
+    grabberConfig.inverted(Constants.Grabber.LEFT_GRABBER_INVERTED);
+    REVUtils.tryUntilOk(() -> leftGrabberMotor.configure(grabberConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
     
-    config.inverted(Constants.Grabber.RIGHT_GRABBER_INVERTED);
-    config.follow(Constants.CAN.GRABBER_LEFT_MOTOR);
-    REVUtils.tryUntilOk(() -> rightGrabberMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+    grabberConfig.follow(Constants.CAN.GRABBER_LEFT_MOTOR, Constants.Grabber.RIGHT_GRABBER_INVERTED);
+    REVUtils.tryUntilOk(() -> rightGrabberMotor.configure(grabberConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+
+    SparkMaxConfig wristConfig = new SparkMaxConfig();
+    // NEO's have a recommended current limit range of 40-60A, but we deliberately lowered it to 38A for our drive
+    // motors. Unclear what this motor limit should be.
+    wristConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(38).voltageCompensation(12.0);
+    REVUtils.tryUntilOk(() -> wristMotor.configure(wristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+
+    // FIXME: This is just a sanity value, but we should figure out homing.
+    wristEncoder.setPosition(0);
   }
 
   @Override 
