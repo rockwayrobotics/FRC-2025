d2eeb7c87ddf257e4304ec0bdb5754c767f78e22
Jeremy Nicholl <nicholljeremy@gmail.com>
2025-03-01 15:28:32 -0500

    Update sensors to use AdvantageKit directly

--- src/main/java/frc/robot/util/Sensors.java
+++ src/main/java/frc/robot/util/Sensors.java
@@ -1,62 +1,45 @@
 package frc.robot.util;
 
-import edu.wpi.first.networktables.BooleanPublisher;
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.Constants;
 
 public class Sensors {
-  private AnalogInput GrabberAcquiredDistanceSensor = new AnalogInput(
+  private AnalogInput grabberAcquiredDistanceSensor = new AnalogInput(
       Constants.Analog.ALGAE_DISTANCE_SENSOR);
-  private DigitalInput GrabberHomeSwitch = new DigitalInput(
+  private DigitalInput grabberHomeSwitch = new DigitalInput(
       Constants.Digital.ALGAE_HOME_LIMIT_SWITCH);
-  private DigitalInput ChuteHomeSwitch = new DigitalInput(Constants.Digital.CHUTE_HOME_LIMIT_SWITCH);
-  private DigitalInput ChuteCoralReadyBeambreak = new DigitalInput(
+  private DigitalInput chuteHomeSwitch = new DigitalInput(Constants.Digital.CHUTE_HOME_LIMIT_SWITCH);
+  private DigitalInput chuteCoralReadyBeambreak = new DigitalInput(
       Constants.Digital.CHUTE_SHOOT_CORAL_BEAMBREAK);
-  private DigitalInput ChuteCoralLoadedBeambreak = new DigitalInput(
+  private DigitalInput chuteCoralLoadedBeambreak = new DigitalInput(
       Constants.Digital.CHUTE_LOAD_CORAL_BEAMBREAK);
   // private DigitalInput ChuteLoadCoralBeambreak = new DigitalInput(3);
-  private DigitalInput ElevatorHomeBeambreak = new DigitalInput(
+  private DigitalInput elevatorHomeBeambreak = new DigitalInput(
       Constants.Digital.ELEVATOR_HOME_BEAMBREAK);
 
-  private DoublePublisher GrabberAcquiredDistanceSensorPublisher;
-  private BooleanPublisher GrabberHomeSwitchPublisher;
-  private BooleanPublisher ChuteHomeSwitchPublisher;
-  private BooleanPublisher ChuteShootCoralBeambreakPublisher;
-  private BooleanPublisher ChuteLoadCoralBeambreakPublisher;
-  private BooleanPublisher ElevatorHomeBeambreakPublisher;
-
-  private static Sensors Instance = null;
+  private static Sensors instance = null;
 
   private Sensors() {
-    var nt = NetworkTableInstance.getDefault();
-    this.GrabberAcquiredDistanceSensorPublisher = nt
-        .getDoubleTopic("/AdvantageKit/RealOutputs/Grabber/acquired_distance_mm").publish();
-    this.GrabberHomeSwitchPublisher = nt.getBooleanTopic("/AdvantageKit/RealOutputs/Grabber/home_sw").publish();
-    this.ChuteHomeSwitchPublisher = nt.getBooleanTopic("/AdvantageKit/RealOutputs/Chute/home_sw").publish();
-    this.ChuteShootCoralBeambreakPublisher = nt.getBooleanTopic("/AdvantageKit/RealOutputs/Chute/ready_sw").publish();
-    this.ChuteLoadCoralBeambreakPublisher = nt.getBooleanTopic("/AdvantageKit/RealOutputs/Chute/loaded_sw").publish();
-    this.ElevatorHomeBeambreakPublisher = nt.getBooleanTopic("/AdvantageKit/RealOutputs/Elevator/home_sw").publish();
-
     updateNT();
   }
 
   public static Sensors getInstance() {
-    if (Instance == null) {
-      Instance = new Sensors();
+    if (instance == null) {
+      instance = new Sensors();
     }
-    return Instance;
+    return instance;
   }
 
   public void updateNT() {
-    this.GrabberAcquiredDistanceSensorPublisher.set(getGrabberAcquiredDistance());
-    this.GrabberHomeSwitchPublisher.set(getGrabberHomeSwitch());
-    this.ChuteHomeSwitchPublisher.set(getChuteHomeSwitch());
-    this.ChuteShootCoralBeambreakPublisher.set(getChuteCoralReadyBeambreak());
-    this.ChuteLoadCoralBeambreakPublisher.set(getChuteCoralLoadedBeambreak());
-    this.ElevatorHomeBeambreakPublisher.set(getElevatorHomeBeambreak());
+    Logger.recordOutput("Grabber/distance_mm", getGrabberAcquiredDistance());
+    Logger.recordOutput("Grabber/home_sw", getGrabberHomeSwitch());
+    Logger.recordOutput("Chute/home_sw", getChuteHomeSwitch());
+    Logger.recordOutput("Chute/loaded_sw", getChuteCoralLoadedBeambreak());
+    Logger.recordOutput("Chute/ready_sw", getChuteCoralReadyBeambreak());
+    Logger.recordOutput("Elevator/home_sw", getElevatorHomeBeambreak());
   }
 
   /**
@@ -66,31 +49,31 @@ public class Sensors {
   public double getGrabberAcquiredDistance() {
     // TODO: Calibrate this and convert from voltage to mm.
     // 40 to 300 mm
-    return GrabberAcquiredDistanceSensor.getVoltage();
+    return grabberAcquiredDistanceSensor.getVoltage();
   }
 
   /** @return true if the grabber home limit switch is pressed. */
   public boolean getGrabberHomeSwitch() {
-    return GrabberHomeSwitch.get();
+    return grabberHomeSwitch.get();
   }
 
   /** @return true if the chute home limit switch is pressed. */
   public boolean getChuteHomeSwitch() {
-    return ChuteHomeSwitch.get();
+    return chuteHomeSwitch.get();
   }
 
-  /** @return talse when beam is broken */
+  /** @return false when beam is broken */
   public boolean getChuteCoralReadyBeambreak() {
-    return ChuteCoralReadyBeambreak.get();
+    return chuteCoralReadyBeambreak.get();
   }
 
-  /** @return talse when beam is broken */
+  /** @return false when beam is broken */
   public boolean getChuteCoralLoadedBeambreak() {
-    return ChuteCoralLoadedBeambreak.get();
+    return chuteCoralLoadedBeambreak.get();
   }
 
-  /** @return talse when elevator is home */
+  /** @return false when elevator is home */
   public boolean getElevatorHomeBeambreak() {
-    return ElevatorHomeBeambreak.get();
+    return elevatorHomeBeambreak.get();
   }
 }
