737fe859b091fac39182d263b436ce49784116f3
Peter Hansen <peter@engcorp.com>
2025-03-03 00:19:53 -0500

    continue building cam module, mostly cleanup still

--- vision/cam/cam.py
+++ vision/cam/cam.py
@@ -1,42 +1,43 @@
-#!/usr/bin/env python3
+'''Camera classes'''
 
-# TODO: camera calibration https://learnopencv.com/camera-calibration-using-opencv/
+# Reference camera calibration https://learnopencv.com/camera-calibration-using-opencv/
 # see also https://github.com/tobybreckon/python-examples-cv/blob/master/calibrate_camera.py
 # chessboard: https://docs.opencv.org/3.4/pattern.png
 
-import datetime as dt
-import io
-import logging
-import threading
+# pylint: disable=missing-function-docstring,missing-class-docstring,no-member
+
 import math
-import os
-import platform
-import time
-import traceback
+from pathlib import Path
+
+import cv2
 
 try:
     from libcamera import Transform
     import picamera2
-    pi = True
+    PI = True
 except ImportError:
-    pi = False
+    PI = False
 
 
-degrees = lambda rad: rad * 180 / math.pi
+def degrees(rad):
+    '''Convert radians to degrees.'''
+    return rad * 180 / math.pi
+
 
 class PiCam:
-    def __init__(self, args, id, fps=30):
-        self.cam = picamera2.Picamera2(id)
-        self.id = id
+    '''Raspberry Pi camera (use MockCam if not on a Pi)'''
+    def __init__(self, num, res, fps=30):
+        self.cam = picamera2.Picamera2(num)
+        self.num = num
 
         # Default exposure time is 22162 us
         # To get fast fps, need Exposure time lower, buffer_count > 1 (4 is
         # better than 2), and FrameDurationLimits maybe (slight difference)
         vid1 = self.cam.create_video_configuration(
-            main=dict(size=SIZE, format='RGB888'),
-            lores=dict(size=[320,240], format='RGB888'),
-            controls=dict(ExposureTime=11081, FrameRate=args.fps,
-                          FrameDurationLimits=(1,17000)), # FrameDurationLimits=(1, 5000)),
+            main={"size": res, "format": "RGB888"},
+            lores={"size": [320, 240], "format": "RGB888"},
+            controls={"ExposureTime": 11081, "FrameRate": fps,
+                      "FrameDurationLimits": (1, 17000)},  # FrameDurationLimits=(1, 5000)),
             queue=False,
             buffer_count=4,
             transform=Transform(hflip=1, vflip=1),
@@ -47,39 +48,47 @@ class PiCam:
         # print('Cam config:\n%s' % '\n'.join(f'{x:>15} = {y}' for x, y in cam_config.items()))
         self.cam.configure(cam_config)
 
-    def capture_array(self, config):
+    def capture_array(self, config='main'):
+        '''Return latest frame as a numpy array. May block if frame not ready.'''
         return self.cam.capture_array(config)
 
     def stop(self):
+        '''Stop the camera subsystem from capturing frames.'''
         self.cam.stop()
-    
+
     def start(self):
+        '''Start the camera capturing frames.'''
         self.cam.start()
 
+
 class MockCam:
-    def __init__(self, args, id):
-        self.id = id
+    # pylint: disable=unused-argument
+    def __init__(self, num, res, fps=30):
+        self.num = num
 
-        import cv2
-        image = cv2.imread('camera-fallbacks/cam0.jpg' if self.id == 0 else 'camera-fallbacks/cam1.jpg')
-        self.resized = cv2.resize(image, (SIZE[0], SIZE[1]))
-        self.resizedLowRes = cv2.resize(image, (320, 240))
+        imgpath = Path(__file__).parent.absolute() / f'cam{self.num}.jpg'
+        image = cv2.imread(imgpath)
+        self._main = cv2.resize(image, (res[0], res[1]))
+        self._lores = cv2.resize(image, (320, 240))
 
-    def capture_array(self, config):
-        if config == 'main':
-            return self.resized
-        elif config == 'lores':
-            return self.resizedLowRes
-        
+    def capture_array(self, config='main'):
+        return self._main if config == 'main' else self._lores
+
     def stop(self):
         pass
-    
+
     def start(self):
         pass
 
 
-if pi:
+if PI:
     Camera = PiCam
 else:
     Camera = MockCam
-    
+
+if __name__ == '__main__':
+    cam = Camera(0, (640, 480))
+    cam.start()
+    img = cam.capture_array()
+    cam.stop()
+    print(img.shape)
--- vision/cam/main.py
+++ vision/cam/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# TODO: camera calibration https://learnopencv.com/camera-calibration-using-opencv/
-# see also https://github.com/tobybreckon/python-examples-cv/blob/master/calibrate_camera.py
-# chessboard: https://docs.opencv.org/3.4/pattern.png
+# pylint: disable=unused-import,invalid-name,trailing-whitespace,missing-module-docstring
+# pylint: disable=missing-function-docstring,missing-class-docstring,no-member
+# pylint: disable=consider-using-with,too-few-public-methods
 
 import datetime as dt
 import io
@@ -19,8 +19,6 @@ from cscore import CvSource, VideoMode, CameraServer, MjpegServer
 
 import numpy as np
 
-from keys import NonBlockingConsole
-
 import cv2
 import robotpy_apriltag as at
 
@@ -28,63 +26,16 @@ from ntcore import NetworkTableInstance, PubSubOptions
 from wpimath.geometry import CoordinateSystem, Transform3d, Translation3d, Rotation3d
 from wpimath import units
 
-from libcamera import Transform
-import picamera2
+try:
+    from libcamera import Transform
+    import picamera2
+    PI = True
+except ImportError:
+    PI = False
 
+from . import cam
+from .keys import NonBlockingConsole
 
-degrees = lambda rad: rad * 180 / math.pi
-
-class PiCam:
-    def __init__(self, args, id):
-        self.cam = picamera2.Picamera2(id)
-        self.id = id
-
-        # Default exposure time is 22162 us
-        # To get fast fps, need Exposure time lower, buffer_count > 1 (4 is
-        # better than 2), and FrameDurationLimits maybe (slight difference)
-        vid1 = self.cam.create_video_configuration(
-            main=dict(size=SIZE, format='RGB888'),
-            lores=dict(size=[320,240], format='RGB888'),
-            controls=dict(ExposureTime=11081, FrameRate=args.fps,
-                          FrameDurationLimits=(1,17000)), # FrameDurationLimits=(1, 5000)),
-            queue=False,
-            buffer_count=4,
-            transform=Transform(hflip=1, vflip=1),
-            )
-
-        cam_config = vid1
-        self.cam.align_configuration(cam_config)
-        # print('Cam config:\n%s' % '\n'.join(f'{x:>15} = {y}' for x, y in cam_config.items()))
-        self.cam.configure(cam_config)
-
-    def capture_array(self, config):
-        return self.cam.capture_array(config)
-
-    def stop(self):
-        self.cam.stop()
-    
-    def start(self):
-        self.cam.start()
-
-class FakeCam:
-    def __init__(self, args, id):
-        self.id = id
-
-        image = cv2.imread('camera-fallbacks/cam0.jpg' if self.id == 0 else 'camera-fallbacks/cam1.jpg')
-        self.resized = cv2.resize(image, (SIZE[0], SIZE[1]))
-        self.resizedLowRes = cv2.resize(image, (320, 240))
-
-    def capture_array(self, config):
-        if config == 'main':
-            return self.resized
-        elif config == 'lores':
-            return self.resizedLowRes
-        
-    def stop(self):
-        pass
-    
-    def start(self):
-        pass
 
 class AprilTagDetection:
     def __init__(self, args, nt):
@@ -104,8 +55,9 @@ class AprilTagDetection:
 
         self.field = at.AprilTagFieldLayout("2025-reefscape.json")
         config = at.AprilTagPoseEstimator.Config(
-        tagSize = units.inchesToMeters(6.5), # 16.51cm
-        **CALS.get(args.cals, CALS.get('640x480')))
+            tagSize = units.inchesToMeters(6.5), # 16.51cm
+            **CALS.get(args.cals, CALS.get('640x480')).get_dict()
+        )
 
         self.estimator = at.AprilTagPoseEstimator(config)
 
@@ -118,7 +70,7 @@ class AprilTagDetection:
         self.ambiguityTopic = nt.getFloatTopic(
             "/Shuffleboard/Drivebase/Field2d/Ambiguity"
         ).publish(PubSubOptions())  # Uses default options
-    
+
     def detect(self, arr):
         # img = arr[:height,:]
         img = cv2.cvtColor(arr, cv2.COLOR_RGB2GRAY)
@@ -149,86 +101,64 @@ class AprilTagDetection:
         return False
 
 
-
-
-# imx219 [3280x2464] (/base/soc/i2c0mux/i2c@1/imx219@10)
-# 'SRGGB10_CSI2P' :  640x480  [206.65 fps - (1000, 752)/1280x960 crop]
-#                   1640x1232 [41.85 fps - (0, 0)/3280x2464 crop]
-#                   1920x1080 [47.57 fps - (680, 692)/1920x1080 crop]
-#                   3280x2464 [21.19 fps - (0, 0)/3280x2464 crop]
-#        'SRGGB8' :  640x480  [206.65 fps - (1000, 752)/1280x960 crop]
-#                   1640x1232 [41.85 fps - (0, 0)/3280x2464 crop]
-#                   1920x1080 [47.57 fps - (680, 692)/1920x1080 crop]
-#                   3280x2464 [21.19 fps - (0, 0)/3280x2464 crop]
-
+class CamCal:
+    def __init__(self, fx, fy, cx, cy):
+        self.array = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1], np.float64)
+        self.value = { "fx":fx, "fy":fy, "cx":cx, "cy": cy }
+    def get_array(self):
+        return self.array
+    def get_dict(self):
+        return self.value
 
 CALS = {
-    # images1
-    # fx = 717.428307,
-    # fy = 712.943769,
-    # cx = 365.509738,
-    # cy = 219.080481,
-
     # images2 640x480
-    '640x480': dict(
-        fx = 813.002665,
-        fy = 814.367913,
-        cx = 340.340811,
-        cy = 248.727651,
-    ),
+    '640x480': CamCal(fx=813.002665, fy=814.367913, cx=340.340811, cy=248.727651),
 
     # images4 1456x1088
-    '1456x1088': dict(
-        fx = 1757.669488,
-        fy = 1762.782233,
-        cx = 736.690867,
-        cy = 557.428635,
-    ),
+    '1456x1088': CamCal(fx=1757.669488, fy=1762.782233, cx=736.690867, cy=557.428635),
 }
 
 def cam2Field(fieldPos, camPos):
-    tagInCameraFrame = Transform3d(camPos.x, camPos.y, camPos.z, Rotation3d(-camPos.rotation().x - math.pi, -camPos.rotation().y, camPos.rotation().z - math.pi))
+    tagInCameraFrame = Transform3d(camPos.x, camPos.y, camPos.z,
+       Rotation3d(-camPos.rotation().x - math.pi,
+                  -camPos.rotation().y, camPos.rotation().z - math.pi))
     # Convert from East-Down-North to North-West-Up
-    tagInCameraFrameNWU = CoordinateSystem.convert(tagInCameraFrame, CoordinateSystem.EDN(), CoordinateSystem.NWU())
-    # Add the field-relative tag position to the inverse of the camera-to-tag transform and return it
+    tagInCameraFrameNWU = CoordinateSystem.convert(
+        tagInCameraFrame, CoordinateSystem.EDN(), CoordinateSystem.NWU())
+
+    # Add field-relative tag position to the inverse of the camera-to-tag transform and return it.
     result = (fieldPos + tagInCameraFrameNWU.inverse()).toPose2d()
     return result
 
+
 def main():
     args = get_args()
-    try:
-        cam0 = PiCam(args, 0)
-    except Exception:
-        traceback.print_exc()
-        cam0 = FakeCam(args, 0)
+    res = args.res
 
-    try:
-        cam1 = PiCam(args, 1)
-    except Exception:
-        traceback.print_exc()
-        cam1 = FakeCam(args, 1)
+    cam0 = cam.Camera(0, args.res, args.fps)
+    cam1 = cam.Camera(1, args.res, args.fps)
 
-    cam = cam0
+    driver_cam = cam0
 
     nt = NetworkTableInstance.getDefault()
     nt.setServerTeam(8089)
     nt.startClient4("pi cam")
 
-    source = CvSource("cam", VideoMode.PixelFormat.kBGR, SIZE[0], SIZE[1], int(args.fps))
+    source = CvSource("cam", VideoMode.PixelFormat.kBGR, res[0], res[1], int(args.fps))
     mjpegServer = MjpegServer("mjpeg", 8081)
     mjpegServer.setSource(source)
     mjpegTopic = nt.getStringArrayTopic("/CameraPublisher/PiCam/streams").publish(PubSubOptions())
 
     selectCameraTopic = nt.getStringTopic("/CameraPublisher/PiCam/selected")
-    selectCameraPublisher = selectCameraTopic.publish(PubSubOptions())
-    selectCameraPublisher.set("fore")
-    selectCameraSubscriber = selectCameraTopic.subscribe("fore")
+    selectCameraPub = selectCameraTopic.publish(PubSubOptions())
+    selectCameraPub.set("fore")
+    selectCameraSub = selectCameraTopic.subscribe("fore")
 
     camAutoDeadbandTopic = nt.getDoubleTopic("/Tuning/Camera Auto Deadband")
-    camAutoDeadbandSubscriber = camAutoDeadbandTopic.subscribe(0)    
+    camAutoDeadbandSub = camAutoDeadbandTopic.subscribe(0)    
 
-    rightVelocitySubscriber = nt.getDoubleTopic("/AdvantageKit/Drive/RightVelocityMetersPerSec").subscribe(0)
-    leftVelocitySubscriber = nt.getDoubleTopic("/AdvantageKit/Drive/LeftVelocityMetersPerSec").subscribe(0)
+    rightVelocitySub = nt.getDoubleTopic("/AdvantageKit/Drive/RightVelocityMetersPerSec").subscribe(0)
+    leftVelocitySub = nt.getDoubleTopic("/AdvantageKit/Drive/LeftVelocityMetersPerSec").subscribe(0)
 
     mjpegTopic.set(["mjpg:http://10.80.89.11:8081/?action=stream"])
 
@@ -239,8 +169,6 @@ def main():
 
     # mjpegServer = CameraServer.startAutomaticCapture(source)
     # print(mjpegServer.getPort())
-
-    # time.sleep(0.1)
 
     console = NonBlockingConsole()
 
@@ -253,26 +181,26 @@ def main():
         reported = start
         count = 0
         fps = 0
-        height = SIZE[1] * 2 // 3
-        print('SIZE', SIZE, 'height', height, end='\r\n')
+        height = res[1] * 2 // 3
+        print('res', res, 'height', height, end='\r\n')
 
         while now - start < args.time:
-            currentCam = selectCameraSubscriber.get()
+            currentCam = selectCameraSub.get()
             if currentCam == 'fore':
-                cam = cam0
+                driver_cam = cam0
             elif currentCam == 'aft':
-                cam = cam1
+                driver_cam = cam1
             elif currentCam == 'auto':
-                leftVelocity = leftVelocitySubscriber.get()
-                rightVelocity = rightVelocitySubscriber.get()
-                deadbandThreshold = camAutoDeadbandSubscriber.get()
+                leftVelocity = leftVelocitySub.get()
+                rightVelocity = rightVelocitySub.get()
+                deadbandThreshold = camAutoDeadbandSub.get()
                 #print(f'deadbandThreshold={deadbandThreshold}', end = '\r\n')
 
                 if abs(leftVelocity) > deadbandThreshold and abs(rightVelocity) > deadbandThreshold:
                     if leftVelocity > 0 and rightVelocity > 0:
-                        cam = cam0
+                        driver_cam = cam0
                     elif leftVelocity < 0 and rightVelocity < 0:
-                        cam = cam1
+                        driver_cam = cam1
 
             count += 1
             now = time.time()
@@ -308,13 +236,13 @@ def main():
                 if key.lower() == 'q':
                     break
                 elif key.lower() == 'c':
-                    if cam == cam0:
-                        cam = cam1
-                        selectCameraPublisher.set("aft")
+                    if driver_cam == cam0:
+                        driver_cam = cam1
+                        selectCameraPub.set("aft")
                     else:
-                        cam = cam0
-                        selectCameraPublisher.set("fore")
-                    print(f'Switched to camera {cam.id}')
+                        driver_cam = cam0
+                        selectCameraPub.set("fore")
+                    print(f'Switched to camera {driver_cam.num}')
 
     finally:
         del console # this should restore original kb settings
@@ -341,6 +269,7 @@ def main():
 
 
 def get_args():
+    # pylint: disable=import-outside-toplevel
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true')
@@ -356,11 +285,10 @@ def get_args():
     # This is a bad way to do this, shoving these into the global
     # namespace, but it's a temporary hack to avoid having to change some
     # of the original code which did that as a shortcut.
-    global args, SIZE
     args = parser.parse_args()
     if args.cals is None:
         args.cals = args.res
-    SIZE = tuple(int(x) for x in args.res.split('x'))
+    args.res = tuple(int(x) for x in args.res.split('x'))
     return args
 
 
