d7aea9a8ab20b53ccee0c13da60fda2caba327d5
Jeremy Nicholl <nicholljeremy@gmail.com>
2025-03-02 21:05:59 -0500

    Add part of climp tuners

--- src/main/java/frc/robot/subsystems/climp/ClimpIOReal.java
+++ src/main/java/frc/robot/subsystems/climp/ClimpIOReal.java
@@ -12,18 +12,28 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.robot.Constants;
 import frc.robot.util.REVUtils;
+import frc.robot.util.Tuner;
 
 public class ClimpIOReal implements ClimpIO {
   protected final SparkFlex climpMotor = new SparkFlex(Constants.CAN.CLIMP_MOTOR, MotorType.kBrushless);
 
+  final Tuner pid_p = new Tuner("Climp/Kp", 0, true);
+  final Tuner pid_d = new Tuner("Climp/Kd", 0, true);
+  final Tuner currentLimit = new Tuner("Climp/current_limit", 5.0, true);
+  final Tuner maxNormalizedSpeed = new Tuner("Climp/normalized_speed_max", 0.1, true);
+  final Tuner minNormalizedSpeed = new Tuner("Climp/normalized_speed_min", -0.1, true);
+
   protected final RelativeEncoder encoder = climpMotor.getEncoder();
-  
+
   public ClimpIOReal() {
-    SparkFlexConfig config = new SparkFlexConfig();
-    config.idleMode(IdleMode.kBrake);
-    
-    REVUtils.tryUntilOk(() -> climpMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
-  }  
+    updateParams(true);
+
+    pid_p.addListener((_e) -> updateParams(false));
+    pid_d.addListener((_e) -> updateParams(false));
+    currentLimit.addListener((_e) -> updateParams(false));
+    maxNormalizedSpeed.addListener((_e) -> updateParams(false));
+    minNormalizedSpeed.addListener((_e) -> updateParams(false));
+  }
 
   @Override
   public void updateInputs(ClimpIOInputs inputs) {
@@ -33,11 +43,24 @@ public class ClimpIOReal implements ClimpIO {
         climpMotor::getAppliedOutput, climpMotor::getBusVoltage
     }, (values) -> inputs.appliedVoltage = values[0] * values[1]);
     REVUtils.ifOk(climpMotor, climpMotor::getOutputCurrent, (value) -> inputs.supplyCurrentAmps = value);
-
   }
 
   @Override
   public void setClimpMotor(double speed) {
     climpMotor.set(speed);
+  }
+
+  private void updateParams(boolean resetSafe) {
+    ResetMode resetMode = resetSafe ? ResetMode.kResetSafeParameters : ResetMode.kNoResetSafeParameters;
+    SparkFlexConfig config = new SparkFlexConfig();
+    if (resetSafe) {
+      config.idleMode(IdleMode.kBrake).voltageCompensation(12.0);
+    }
+    config.smartCurrentLimit((int) currentLimit.get());
+    config.closedLoop.pidf(pid_p.get(), 0, pid_d.get(), 0);
+    config.closedLoop.outputRange(minNormalizedSpeed.get(), maxNormalizedSpeed.get());
+
+    REVUtils.tryUntilOk(() -> climpMotor.configure(config, resetMode, PersistMode.kPersistParameters));
+
   }
 }
