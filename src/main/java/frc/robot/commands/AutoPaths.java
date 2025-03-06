f674628ff106ad88fe4b56bd6d314f4898740371
tqbed <66844373+tqbed@users.noreply.github.com>
2025-03-06 15:13:15 -0500

    autowaittime tuner

--- src/main/java/frc/robot/commands/AutoPaths.java
+++ src/main/java/frc/robot/commands/AutoPaths.java
@@ -20,87 +20,91 @@ import frc.robot.Constants.Side;
 import frc.robot.RobotTracker;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.superstructure.Superstructure;
+import frc.robot.util.Tuner;
 
 public class AutoPaths {
 
-  private AutoPaths() {
-  }
-
-  public static Command toReef(Drive drive, Superstructure superstructure) {
-    TrajectoryConfig config = new TrajectoryConfig(Constants.Drive.MAX_SPEED_MPS / 3,
-        Constants.Drive.MAX_ACCEL_MPSS / 10)
-        .setKinematics(RobotTracker.getInstance().getDriveKinematics())
-        .addConstraint(new CentripetalAccelerationConstraint(0.5));
-    // TODO: Add voltage constraint with feedforward: .addConstraint(null);
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(8.5, 7.5, Rotation2d.fromDegrees(180)),
-        List.of(), new Pose2d(3, 4, Rotation2d.fromDegrees(-90)), config);
-    // Maybe 4,6 as intermediate?
-
-    // LTVUnicycleController ltvController = new LTVUnicycleController(0.02);
-    // LTVUnicycleController ltvController = new
-    // LTVUnicycleController(VecBuilder.fill(0.0625, 0.125, 2.0),
-    // VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
-    LTVUnicycleController ltvController = new LTVUnicycleController(VecBuilder.fill(0.125, 0.25, 4.0),
-        VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
-
-    double kP = 1.7;
-    double kI = 0.0;
-    double kD = 0.0;
-    PIDController leftController = new PIDController(kP, kI, kD);
-    PIDController rightController = new PIDController(kP, kI, kD);
-
-    drive.setPose(exampleTrajectory.getInitialPose());
-    return new LTVCommand(exampleTrajectory, () -> RobotTracker.getInstance().getEstimatedPose(), ltvController,
-        drive.getFeedForward(), RobotTracker.getInstance().getDriveKinematics(),
-        () -> drive.getWheelSpeeds(),
-        leftController,
-        rightController,
-        (Double leftVoltage, Double rightVoltage) -> {
-          drive.runOpenLoop(leftVoltage, rightVoltage);
-        }, (pose) -> {
-          drive.setPose(pose);
-        }, drive);
-  }
-
-  public static Command midFarRightL2(Drive drive, Superstructure superstructure) {
-    TrajectoryConfig config = new TrajectoryConfig(Constants.Drive.MAX_SPEED_MPS / 3,
-        Constants.Drive.MAX_ACCEL_MPSS / 10)
-        .setKinematics(RobotTracker.getInstance().getDriveKinematics())
-        .addConstraint(new CentripetalAccelerationConstraint(0.5));
-    // TODO: Add voltage constraint with feedforward: .addConstraint(null);
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(7.464, 4.000, Rotation2d.fromDegrees(180)),
-        List.of(), new Pose2d(5.389, 2.824, Rotation2d.fromDegrees(180)), config);
-    // Maybe 4,6 as intermediate?
-
-    // LTVUnicycleController ltvController = new LTVUnicycleController(0.02);
-    // LTVUnicycleController ltvController = new
-    // LTVUnicycleController(VecBuilder.fill(0.0625, 0.125, 2.0),
-    // VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
-    LTVUnicycleController ltvController = new LTVUnicycleController(VecBuilder.fill(0.125, 0.25, 4.0),
-        VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
-
-    double kP = 1.7;
-    double kI = 0.0;
-    double kD = 0.0;
-    PIDController leftController = new PIDController(kP, kI, kD);
-    PIDController rightController = new PIDController(kP, kI, kD);
-
-    drive.setPose(exampleTrajectory.getInitialPose());
-    LTVCommand path1 = new LTVCommand(exampleTrajectory, () -> RobotTracker.getInstance().getEstimatedPose(),
-        ltvController,
-        drive.getFeedForward(), RobotTracker.getInstance().getDriveKinematics(),
-        () -> drive.getWheelSpeeds(),
-        leftController,
-        rightController,
-        (Double leftVoltage, Double rightVoltage) -> {
-          drive.runOpenLoop(leftVoltage, rightVoltage);
-        }, (pose) -> {
-          drive.setPose(pose);
-        }, drive);
-
-    return Commands.sequence(path1, Commands.runOnce(() -> superstructure.gotoSetpoint(CoralLevel.L2, Side.RIGHT)),
+  // adjustable auto wait time
+  private final static Tuner autoWaitTime = new Tuner("AutoWaitTime", 0.0, false); 
+  
+    private AutoPaths() {
+    }
+  
+    public static Command toReef(Drive drive, Superstructure superstructure) {
+      TrajectoryConfig config = new TrajectoryConfig(Constants.Drive.MAX_SPEED_MPS / 3,
+          Constants.Drive.MAX_ACCEL_MPSS / 10)
+          .setKinematics(RobotTracker.getInstance().getDriveKinematics())
+          .addConstraint(new CentripetalAccelerationConstraint(0.5));
+      // TODO: Add voltage constraint with feedforward: .addConstraint(null);
+      Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+          new Pose2d(8.5, 7.5, Rotation2d.fromDegrees(180)),
+          List.of(), new Pose2d(3, 4, Rotation2d.fromDegrees(-90)), config);
+      // Maybe 4,6 as intermediate?
+  
+      // LTVUnicycleController ltvController = new LTVUnicycleController(0.02);
+      // LTVUnicycleController ltvController = new
+      // LTVUnicycleController(VecBuilder.fill(0.0625, 0.125, 2.0),
+      // VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
+      LTVUnicycleController ltvController = new LTVUnicycleController(VecBuilder.fill(0.125, 0.25, 4.0),
+          VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
+  
+      double kP = 1.7;
+      double kI = 0.0;
+      double kD = 0.0;
+      PIDController leftController = new PIDController(kP, kI, kD);
+      PIDController rightController = new PIDController(kP, kI, kD);
+  
+      drive.setPose(exampleTrajectory.getInitialPose());
+      return new LTVCommand(exampleTrajectory, () -> RobotTracker.getInstance().getEstimatedPose(), ltvController,
+          drive.getFeedForward(), RobotTracker.getInstance().getDriveKinematics(),
+          () -> drive.getWheelSpeeds(),
+          leftController,
+          rightController,
+          (Double leftVoltage, Double rightVoltage) -> {
+            drive.runOpenLoop(leftVoltage, rightVoltage);
+          }, (pose) -> {
+            drive.setPose(pose);
+          }, drive);
+    }
+  
+    public static Command midFarRightL2(Drive drive, Superstructure superstructure) {
+      TrajectoryConfig config = new TrajectoryConfig(Constants.Drive.MAX_SPEED_MPS / 3,
+          Constants.Drive.MAX_ACCEL_MPSS / 10)
+          .setKinematics(RobotTracker.getInstance().getDriveKinematics())
+          .addConstraint(new CentripetalAccelerationConstraint(0.5));
+      // TODO: Add voltage constraint with feedforward: .addConstraint(null);
+      Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+          new Pose2d(7.464, 4.000, Rotation2d.fromDegrees(180)),
+          List.of(), new Pose2d(5.389, 2.824, Rotation2d.fromDegrees(180)), config);
+      // Maybe 4,6 as intermediate?
+  
+      // LTVUnicycleController ltvController = new LTVUnicycleController(0.02);
+      // LTVUnicycleController ltvController = new
+      // LTVUnicycleController(VecBuilder.fill(0.0625, 0.125, 2.0),
+      // VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
+      LTVUnicycleController ltvController = new LTVUnicycleController(VecBuilder.fill(0.125, 0.25, 4.0),
+          VecBuilder.fill(1.0, 2.0), 0.02, config.getMaxVelocity());
+  
+      double kP = 1.7;
+      double kI = 0.0;
+      double kD = 0.0;
+      PIDController leftController = new PIDController(kP, kI, kD);
+      PIDController rightController = new PIDController(kP, kI, kD);
+  
+      drive.setPose(exampleTrajectory.getInitialPose());
+      LTVCommand path1 = new LTVCommand(exampleTrajectory, () -> RobotTracker.getInstance().getEstimatedPose(),
+          ltvController,
+          drive.getFeedForward(), RobotTracker.getInstance().getDriveKinematics(),
+          () -> drive.getWheelSpeeds(),
+          leftController,
+          rightController,
+          (Double leftVoltage, Double rightVoltage) -> {
+            drive.runOpenLoop(leftVoltage, rightVoltage);
+          }, (pose) -> {
+            drive.setPose(pose);
+          }, drive);
+  
+      return Commands.sequence(Commands.waitSeconds(autoWaitTime.get()), path1, Commands.runOnce(() -> superstructure.gotoSetpoint(CoralLevel.L2, Side.RIGHT)),
         Commands.runOnce(() -> superstructure.startShooting()));
   }
 }
